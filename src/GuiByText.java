0f4b33b819460c62707634023a1d821019d50e1a
stwahyuu <septiantw27@gmail.com>
2021-06-12 20:32:33 +0700

    membuat label dan textfield

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,12 +1,28 @@
 import javax.swing.*;
 
 public class GuiByText extends JFrame {
+    JLabel label1 = new JLabel("Coba Input 1");
+    JTextField textField1 = new JTextField();
+
     GuiByText(){
         setSize(640,480);
+        setLocationRelativeTo(null);
+        setTitle("GUI By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    void  meletakkanKomponen(){
+        getContentPane().setLayout(null);
+        getContentPane().add(label1);
+        getContentPane().add(textField1);
+
+        label1.setBounds(16, 32, 100, 25);
+        textField1.setBounds(124, 32, 100, 25);
+
     }
 
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
     }
 }
